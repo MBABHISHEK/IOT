84d9902205906f13b25c291f87389fdefc2ba077
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2024-05-27 12:11:36 +0530

    Update humidity.ino

--- 2/a/humidity.ino
+++ 2/a/humidity.ino
@@ -1,63 +1,31 @@
-int DHpin = 8; // input/output pin
-byte dat[5];   
-
-byte read_data()
-{
-  byte i = 0;
-  byte result = 0;
-  for (i = 0; i < 8; i++) {
-      while (digitalRead(DHpin) == LOW); // wait 50us
-      delayMicroseconds(30); //The duration of the high level is judged to determine whether the data is '0' or '1'
-      if (digitalRead(DHpin) == HIGH)
-        result |= (1 << (8 - i)); //High in the former, low in the post
-    while (digitalRead(DHpin) == HIGH); //Data '1', waiting for the next bit of reception
-    }
-  return result;
-}
-
-void start_test()
-{
-  digitalWrite(DHpin, LOW); //Pull down the bus to send the start signal
-  delay(30); //The delay is greater than 18 ms so that DHT 11 can detect the start signal
-  digitalWrite(DHpin, HIGH);
-  delayMicroseconds(40); //Wait for DHT11 to respond
-  pinMode(DHpin, INPUT);
-  while(digitalRead(DHpin) == HIGH);
-  delayMicroseconds(80); //The DHT11 responds by pulling the bus low for 80us;
-  
-  if(digitalRead(DHpin) == LOW)
-    delayMicroseconds(80); //DHT11 pulled up after the bus 80us to start sending data;
-  for(int i = 0; i < 5; i++) //Receiving temperature and humidity data, check bits are not considered;
-    dat[i] = read_data();
-  pinMode(DHpin, OUTPUT);
-  digitalWrite(DHpin, HIGH); //After the completion of a release of data bus, waiting for the host to start the next signal
-}
-
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(DHpin, OUTPUT);
-}
-
-void loop()
-{
-  start_test();
-  Serial.print("Humdity = ");
-  Serial.print(dat[0], DEC); //Displays the integer bits of humidity;
-  Serial.print('.');
-  Serial.print(dat[1], DEC); //Displays the decimal places of the humidity;
-  Serial.println('%');
-  Serial.print("Temperature = ");
-  Serial.print(dat[2], DEC); //Displays the integer bits of temperature;
-  Serial.print('.');
-  Serial.print(dat[3], DEC); //Displays the decimal places of the temperature;
-  Serial.println('C');
-
-  byte checksum = dat[0] + dat[1] + dat[2] + dat[3];
-  if (dat[4] != checksum) 
-    Serial.println("-- Checksum Error!");
-  else
-    Serial.println("-- OK");
+#include "dht.h"
+#define dht_apin A0 // Analog Pin sensor is connected to
  
-  delay(1000);
-}
+dht DHT;
+ 
+void setup(){
+ 
+  Serial.begin(9600);
+  delay(500);//Delay to let system boot
+  Serial.println("DHT11 Humidity & temperature Sensor\n\n");
+  delay(1000);//Wait before accessing Sensor
+ 
+}//end "setup()"
+ 
+void loop(){
+  //Start of Program 
+ 
+    DHT.read11(dht_apin);
+    
+    Serial.print("Current humidity = ");
+    Serial.print(DHT.humidity);
+    Serial.print("%  ");
+    Serial.print("temperature = ");
+    Serial.print(DHT.temperature); 
+    Serial.println("C  ");
+    
+    delay(5000);//Wait 5 seconds before accessing sensor again.
+ 
+  //Fastest should be once every two seconds.
+ 
+}// end loop() 
